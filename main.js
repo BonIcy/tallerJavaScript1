d727be57ec39cc1100e01c39889559a23806f254
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-10 09:03:55 -0500

    feat: :ambulance: ejercicio 7
    se soluciono el ejercicio 7 con un do while

--- main.js
+++ main.js
@@ -1,30 +1,16 @@
-/* 6. Construir el algoritmo en Javascript para un programa
-para cualquier cantidad de estudiantes que lea el nombre,
-el sexo y la nota definitiva y halle al estudiante con la mayor
-nota y al estudiante con la menor nota y cuantos eran
-hombres y cuantos mujeres. */
-let nombs = [];
-let nota = [];
-let hombres= 0;
-let mujeres = 0;
+/* 7. Programa que pida el ingreso del nombre y precio de un artículo y la
+cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
+en su factura. */
+let fTotal = 0;
+preTotal = parseFloat("");
 do{
-    let nomb = prompt("Ingresa un nombre");
-    nombs.unshift(nomb);
-    nota.unshift(parseFloat(prompt("Ingrese su nota (de 0 a 5)")));
-    sexo = prompt("Ingresa el sexo (M para masculino, F para femenino");
-    
-    if(sexo = "M"){
-        hombres++;
-    }
-    else if (sexo = "F"){
-        mujeres++;
-    }
-}while(confirm("Deseas guardar otro dato?"));
+    let nombr = prompt("Ingresa tu nombre:");
+    let nart = prompt(nombr + ", ingresa el nombre del articulo");
+    let part = parseFloat(prompt("Ingresa el precio por unidad del o de los " + nart + "(s)"));
+    let cart = parseFloat(prompt("Digita el numero de "+ nart + "(s) que desea llevar"));
+    preTotal = parseFloat(part*cart);
+    fTotal += preTotal;
+    alert("El precio total a pagar por los " + cart + " " + nart + "(s)" + " llevados es de" + " COP$" + preTotal);
+}while(confirm("¿Deseas añadir otro articulo?"));
 
-
-let pmax = nota.indexOf(Math.max(...nota));
-console.log(nombs[pmax], " fue la persona con la nota mas alta");
-
-let pmin = nota.indexOf(Math.min(...nota));
-console.log(nombs[pmin], " fue la persona con la nota mas baja, jaja");
-console.log("Hay " + hombres + " hombre(s) en el programa. \nHay " + mujeres + " mujer(es) en el programa" )
+alert("Tu valor total a cancelar es de COP($)" + fTotal)
