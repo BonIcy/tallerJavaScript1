361e5796a667521335863e3da6b92c0f586e3bb1
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-09 21:07:28 -0500

    feat: :ambulance: ejercicio 5
    se realizo el ejercicio 5

--- main.js
+++ main.js
@@ -1,29 +1,26 @@
-/* 4. Construir el algoritmo que solicite el nombre y edad de 3
-personas y determine el nombre de la persona con mayor edad. */
-n1 = prompt("Ingresa tu nombre");
-e1 = prompt (n1 + ", ingresa tu edad");
-parseInt(e1);
-alert(n1 + " tu edad es de " + e1 + " años");
-n2 = prompt("Ingresa tu nombre");
-e2 = prompt (n2 + ", ingresa tu edad");
-parseInt(e2);
-alert(n2 + " tu edad es de " + e2 + " años");
-n3 = prompt("Ingresa tu nombre");
-e3 = prompt (n3 + ", ingresa tu edad");
-parseInt(e3);
-alert(n3 + " tu edad es de " + e3 + " años");
-let emayor = "";
-if(e1 > e2 && e1 > e3){
-        emayor = n1 + " es la persona con mas edad aqui.";
-    }
-else if(e2 > e1 && e2 > e3){
-        emayor = n2 + " es la persona con mas edad aqui.";
-    }
-else if(e3 > e1 && e3 > e2){
-        emayor = n3 + " es la persona con mas edad aqui.";
-    }
+/* 5. Construir el algoritmo que lea por teclado dos números,
+si el primero es mayor al segundo informar su suma y
+diferencia, en caso contrario, informar el producto y la
+división del primero respecto al segundo. */
+n1 = prompt("Ingresa el primer número");
+n2 = prompt("Ingresa el segundo número");
+parseFloat(n1);
+parseFloat(n2);
+if(n1 > n2){
+    alert("La suma de los digitos da como resultado " + (parseFloat(n1) + parseFloat(n2)) + "\nLa resta de los digitos da como resultado " + (parseFloat(n1) - parseFloat(n2)));
+    setTimeout(function(){
+        window.location.reload();
+     }, 1000);
+}
+else if(n1 < n2){
+    alert("El producto entre los digitos da como resultado "+ (parseFloat(n1) * parseFloat(n2)) + "\nLa division da como resultado " + (parseFloat(n1) / parseFloat(n2)).toFixed(3));
+    setTimeout(function(){
+        window.location.reload();
+     }, 1000);
+}
 else{
-        emayor = "Digita valores validos..."
-    }
-
-alert(emayor)
+    alert("Digita valores validos");
+    setTimeout(function(){
+        window.location.reload();
+     }, 1000);
+}
