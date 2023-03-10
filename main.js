27c53ee87a51af2c40d3397d614b948307c84eba
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-10 08:27:41 -0500

    feat: :ambulance: ejercicio 6
    ejercicio 6 hecho con do while

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
-/* 3. Construir el algoritmo para determinar el voltaje de un
-circuito a partir de la resistencia y la intensidad de corriente.*/
-a = prompt("Inserte el valor que tiene la resistencia (en Ohmios Ω)");
-b = prompt("Digita la intensidad de la corriente (en voltios)");
-parseFloat(a);
-parseFloat(b);
-function operacion (b, a){
-    return b*a;
-}
-var voltaje = operacion(b, a);
+/* 6. Construir el algoritmo en Javascript para un programa
+para cualquier cantidad de estudiantes que lea el nombre,
+el sexo y la nota definitiva y halle al estudiante con la mayor
+nota y al estudiante con la menor nota y cuantos eran
+hombres y cuantos mujeres. */
+let nombs = [];
+let nota = [];
+let hombres= 0;
+let mujeres = 0;
+do{
+    let nomb = prompt("Ingresa un nombre");
+    nombs.unshift(nomb);
+    nota.unshift(parseFloat(prompt("Ingrese su nota (de 0 a 5)")));
+    sexo = prompt("Ingresa el sexo (M para masculino, F para femenino");
+    
+    if(sexo = "M"){
+        hombres++;
+    }
+    else if (sexo = "F"){
+        mujeres++;
+    }
+}while(confirm("Deseas guardar otro dato?"));
 
-alert("Segun la resistencia de " + a +  "Ω aplicada en la intensidad de corriente de " + b + " voltios, el voltaje total del circuito es de " + voltaje +"V.")
+
+let pmax = nota.indexOf(Math.max(...nota));
+console.log(nombs[pmax], " fue la persona con la nota mas alta");
+
+let pmin = nota.indexOf(Math.min(...nota));
+console.log(nombs[pmin], " fue la persona con la nota mas baja, jaja");
+console.log("Hay " + hombres + " hombre(s) en el programa. \nHay " + mujeres + " mujer(es) en el programa" )
