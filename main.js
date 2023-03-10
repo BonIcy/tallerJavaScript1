bd0bec162c2ba9ea0581bb2596c6057424f1bd55
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-09 20:50:34 -0500

    feat: :ambulance: ejercicio 4
    se realizo el ejercicio 4

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
-/* 3. Construir el algoritmo para determinar el voltaje de un
-circuito a partir de la resistencia y la intensidad de corriente.*/
-a = prompt("Inserte el valor que tiene la resistencia (en Ohmios Ω)");
-b = prompt("Digita la intensidad de la corriente (en voltios)");
-parseFloat(a);
-parseFloat(b);
-function operacion (b, a){
-    return b/a;
-}
-var voltaje = operacion(b, a);
-voltajee = (voltaje.toFixed(3));
-alert("Segun la resistencia de " + a +  "Ω aplicada en la intensidad de corriente de " + b + " voltios, el voltaje total del circuito es de " + voltajee +"V.")
+/* 4. Construir el algoritmo que solicite el nombre y edad de 3
+personas y determine el nombre de la persona con mayor edad. */
+n1 = prompt("Ingresa tu nombre");
+e1 = prompt (n1 + ", ingresa tu edad");
+parseInt(e1);
+alert(n1 + " tu edad es de " + e1 + " años");
+n2 = prompt("Ingresa tu nombre");
+e2 = prompt (n2 + ", ingresa tu edad");
+parseInt(e2);
+alert(n2 + " tu edad es de " + e2 + " años");
+n3 = prompt("Ingresa tu nombre");
+e3 = prompt (n3 + ", ingresa tu edad");
+parseInt(e3);
+alert(n3 + " tu edad es de " + e3 + " años");
+let emayor = "";
+if(e1 > e2 && e1 > e3){
+        emayor = n1 + " es la persona con mas edad aqui.";
+    }
+else if(e2 > e1 && e2 > e3){
+        emayor = n2 + " es la persona con mas edad aqui.";
+    }
+else if(e3 > e1 && e3 > e2){
+        emayor = n3 + " es la persona con mas edad aqui.";
+    }
+else{
+        emayor = "Digita valores validos..."
+    }
+
+alert(emayor)
