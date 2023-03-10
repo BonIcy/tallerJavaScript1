b9a8030172e04edb5eb4c817f6348d21568f44b6
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-10 13:09:23 -0500

    feat: :ambulance: ejercicio 8
    solucionadas las opeaciones del ejercicio 8

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-/* 7. Programa que pida el ingreso del nombre y precio de un artículo y la
-cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
-en su factura. */
-let fTotal = 0;
-preTotal = parseFloat("");
-do{
-    let nombr = prompt("Ingresa tu nombre:");
-    let nart = prompt(nombr + ", ingresa el nombre del articulo");
-    let part = parseFloat(prompt("Ingresa el precio por unidad del o de los " + nart + "(s)"));
-    let cart = parseFloat(prompt("Digita el numero de "+ nart + "(s) que desea llevar"));
-    preTotal = parseFloat(part*cart);
-    fTotal += preTotal;
-    alert("El precio total a pagar por los " + cart + " " + nart + "(s)" + " llevados es de" + " COP$" + preTotal);
-}while(confirm("¿Deseas añadir otro articulo?"));
+/* 8. Programa que Ingrese por teclado:
+a. el valor del lado de un cuadrado para mostrar por pantalla el
+perímetro del mismo
+b. la base y la altura de un rectángulo para mostrar el área del
+mismo */
 
-alert("Tu valor total a cancelar es de COP($)" + fTotal)
+ladoCua= parseFloat(prompt("Ingresa el valor de un lado del cuadrado (en cm)"));
+base= parseFloat(prompt("Inserta la base del rectangulo (en cm)"));
+altura = parseFloat(prompt("Inserta la altura del rectangulo (en cm)"));
+
+perimetro = ladoCua * 4;
+area= (base*altura);
+alert("El perimetro del cuadrado es igual a " + perimetro + "cm \nEl area del rectangulo es de " + area + " cm2");
