04f8f0735388fb48282101f31274b83915284cb8
JesusEduardoMartinezReyesCAMPUS1 <mrlgsus22@gmail.com>
2023-03-12 10:06:19 -0500

    feat: :ambulance: ejercicio 9
    se soluciono el ejercicio 9 con la misma logica que se soluciono el ejercicio 6

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-/* 8. Programa que Ingrese por teclado:
-a. el valor del lado de un cuadrado para mostrar por pantalla el
-perímetro del mismo
-b. la base y la altura de un rectángulo para mostrar el área del
-mismo */
+/* 9. N atletas han pasado a finales en salto triple en los juegos
+olímpicos femenino de 2022. Diseñe un programa que pida por
+teclado los nombres de cada atleta finalista y a su vez, sus
+marcas del salto en metros. Informar el nombre de la atleta
+campeona que se quede con la medalla de oro y si rompió
+récord, reportar el pago que será de 500 millones. El récord
+esta en 15,50 metros. */
+let nombs = [];
+let marca = [];
+do {
+    let nomb = prompt("Añade el nombre de la atleta");
+    nombs.unshift(nomb);
+    marca.unshift(parseFloat(prompt("Digita la marca de la atleta (en metros)")));
+}while(confirm("¿Deseas añadir otra atleta?"))
 
-ladoCua= parseFloat(prompt("Ingresa el valor de un lado del cuadrado (en cm)"));
-base= parseFloat(prompt("Inserta la base del rectangulo (en cm)"));
-altura = parseFloat(prompt("Inserta la altura del rectangulo (en cm)"));
+let marmax = Math.max(...marca);
+let marcamax = marca.indexOf(Math.max(...marca));
+console.log(nombs[marcamax], "fue la atleta campeona al tener la marca mas alta, con " + Math.max(...marca) + " metros")
 
-perimetro = ladoCua * 4;
-area= (base*altura);
-alert("El perimetro del cuadrado es igual a " + perimetro + "cm \nEl area del rectangulo es de " + area + " cm2");
+if(marmax > 15.50){
+    console.log("FELICITACIONES, HAS SUPERADO EL RECORD DE 15.50m, POR LO TANTO RECIBIRAS UN PAGO DE 500 BOLIVARES :3");
+}
+else if(marmax == 15.50){
+    console.log("Has igualado el record actual, felicitaciones, tendras que esforzarte un poco mas para la proxima");
+}
+else{
+    console.log("Aunque hayas salido campeona actual, no rompiste el record, ja ja ja")
+}
